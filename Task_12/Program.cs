3ee07605b0067f5e676f13b07ecb0fc44119c1e4
Tatiana <tatiana_khantya@mail.ru>
2022-06-27 21:58:25 +0300

    Задачи 11,12

---
+++ Task_12/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+Console.WriteLine("ВВедите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("ВВедите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a%b==0) Console.WriteLine($"Число {a} кратно {b}");
+
+else 
+{int num = a % b;
+Console.WriteLine($"Число {a} не кратно {b}, остаток {num}");
+};
+
+
+
